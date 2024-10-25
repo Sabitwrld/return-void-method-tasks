aeb3229df6864bc6c024c4ec467cf3e538ba98e5
Sabit <ceferovsabit999@gmail.com>
2024-10-25 21:49:26 +0400

    added tasks and slides

---
+++ task1-return-void/task1-return-void/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace task1_return_void
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int nums = Convert.ToInt32(Console.ReadLine());
+
+            int sum = sumOfNums(nums);
+            Console.WriteLine("Sum of numbers (return method): " + sum);
+
+            printSumNums(nums);
+        }
+        public static int sumOfNums(int nums)
+        {
+            int sum = 0;
+            for (int i = 1; i <= nums; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        public static void printSumNums(int nums)
+        {
+            int sum = 0;
+            for (int i = 1; i <= nums; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Sum of numbers (void method): " + sum);
+        }
+    }
+}
---
+++ task2-string/task2-string/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task2_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " Code Academy ";
+
+            Console.WriteLine(removeSpaces(name));
+            
+        }
+        static string removeSpaces(string space)
+        {
+            string nonSpace = "";
+
+            for (int i = 0; i < space.Length; i++)
+            {
+
+                char a =space[i];
+
+                if (space[i] != ' ')
+                {
+                  nonSpace += a;
+                }
+            }
+                 return nonSpace;           
+        }
+    }
+}
